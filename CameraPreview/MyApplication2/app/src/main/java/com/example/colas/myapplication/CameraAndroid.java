4d0f4edd7072bce83cc0fed0117374796299a86e
Benoît <bcolas27@laposte.net>
2015-01-17 22:36:25 +0100

    code java appli prenant une photo. reste a tester car rencontre des problème avec genymotion et l'emulateur ne gère pas la camera

---
+++ CameraPreview/MyApplication2/app/src/main/java/com/example/colas/myapplication/CameraAndroid.java
@@ -0,0 +1,98 @@
+package com.example.colas.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Button;
+
+import java.io.IOException;
+
+
+public class CameraAndroid extends ActionBarActivity {
+    private Camera cameraObject;
+    private ShowCamera showCamera;
+    private ImageView pic;
+
+    public static Camera isCameraAvailiable(){
+        Camera object = null;
+        try {
+            object = Camera.open();
+        }
+        catch (Exception e){
+        }
+        return object;
+    }
+
+    private PictureCallback capturedIt = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
+            if(bitmap==null){
+                Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();
+            }
+            cameraObject.release();
+        }
+    };
+
+    public void snapIt(View view){
+        cameraObject.takePicture(null, null, capturedIt);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_android);
+
+        pic = (ImageView)findViewById(R.id.imageView);
+        cameraObject = isCameraAvailiable();
+        showCamera = new ShowCamera(this, cameraObject);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+        preview.addView(showCamera);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_camera_android, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
