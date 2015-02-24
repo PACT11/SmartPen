ceaee2e5cfddcc47eeca2cc4d0142e4c9547922c
Anthony Hu <anthony.hu@telecom-paristech.fr>
2015-02-24 17:19:28 +0100

    test homographie sur une rotation : concluant !

--- RecalageImage/ApplicationHomographie/ApplicationHomographie.java
+++ RecalageImage/ApplicationHomographie/ApplicationHomographie.java
@@ -1,68 +1,93 @@
-import java.math.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ApplicationHomographie{
 	
+	private static BufferedImage image1, image2;
 	
-	public static BufferedImage partieEntiere(BufferedImage imageBrute){
+	public static void main(String[] args) throws IOException{
+		File fichierOriginal = new File(args[0]+".jpg");
+        String fichierSortie = args[0]+"_homographie2";
+        image1 = ImageIO.read(fichierOriginal);
+        image2 = partieEntiere(image1);
+        creerImage(fichierSortie);      
+
+	}
+	
+	private static void creerImage(String sortie) throws IOException {
+        File file = new File(sortie+".jpg");
+        ImageIO.write(image2, "jpg", file);
+    }
+	
+	private static BufferedImage partieEntiere(BufferedImage imageBrute){
 		
 		int u, v, rouge, vert, bleu, alpha, nouveauPixel;
 		
 		//Definition de la nouvelle image comme une image vierge
-		int imageRecaleeWidth, imageRecaleeHeight, imageRecaleeType;
-        BufferedImage imageRecalee = new BufferedImage(imageRecaleeWidth, imageRecaleeHeight, imageRecaleeType);
+        BufferedImage imageRecalee = new BufferedImage(imageBrute.getWidth(), imageBrute.getHeight(), imageBrute.getType());
 		
-		for(int i=0; i<imagerecaleeWidth; i++) {
-	            for(int j=0; j<imagerecaleeHeight; j++) {  //On parcourt toute l'image
-	            	(u,v) = floor(phiInverse(i,j)+(1/2,1/2)); //Ce sont les coordonnées du pixel dans l'ancienne image
+		for(int i=0; i<imageBrute.getWidth(); i++) {
+	            for(int j=0; j<imageBrute.getHeight(); j++) {  //On parcourt toute l'image
+	            	 //(u,v) = floor(phiInverse(i,j)+(1/2,1/2)); //Ce sont les coordonnees du pixel dans l'ancienne image
 	                //Il manque l'implentation de la fonction phi
 	            	
+	            	u = (int)(i*Math.cos(0.2) + j*Math.sin(-0.2) +0.5);
+	            	v = (int)(-i*Math.sin(-0.2) + j*Math.cos(0.2)+0.5);
 	            	
-	            	//Obtention des couleurs
-	            	rouge = new Color(imageBrute.getRGB(u,v)).getRed();
-	                vert = new Color(imageBrute.getRGB(u,v)).getGreen();
-	                bleu = new Color(imageBrute.getRGB(u,v)).getBlue();
-	                alpha = new Color(imageBrute.getRGB(u,v)).getAlpha();
+	            	if ((u>0) && (u<imageBrute.getWidth()) && (v>0) && (v<imageBrute.getHeight())) {
+	            	
+	            		//Obtention des couleurs
+	            		rouge = new Color(imageBrute.getRGB(u,v)).getRed();
+	            		vert = new Color(imageBrute.getRGB(u,v)).getGreen();
+	            		bleu = new Color(imageBrute.getRGB(u,v)).getBlue();
+	            		alpha = new Color(imageBrute.getRGB(u,v)).getAlpha();
 	            	
 	                
-	                //Coloration de la nouvelle image
-	                nouveauPixel = colorerPixel(alpha, rouge, vert, bleu);
-	                imageRecalee.setRGB(i, j, nouveauPixel);
+	            		//Coloration de la nouvelle image
+	            		nouveauPixel = colorerPixel(alpha, rouge, vert, bleu);
+	            		imageRecalee.setRGB(i, j, nouveauPixel);
+	            	}
 	            }
 		 }       
 	     
-		return imageRecalee
+		return imageRecalee;
 		
 	}
-	
+
 	public static BufferedImage ponderation(BufferedImage imageBrute){
 		
-		int x1, y1, x2, y2, x3, y3, x4, y4, rouge, rouge1, rouge2, rouge3 rouge4, vert, vert1, vert2, vert3, vert4, bleu, bleu1, bleu2, bleu3, bleu4, alpha, alpha1, alpha2, alpha3, alpha4, gris, nouveauPixel;
+		int x1, y1, x2, y2, x3, y3, x4, y4, rouge, rouge1, rouge2, rouge3, rouge4, vert, vert1, vert2, vert3, vert4, bleu, bleu1, bleu2, bleu3, bleu4, alpha, alpha1, alpha2, alpha3, alpha4, gris, nouveauPixel;
 		double u, v, dx, dy;
 		
 		//Definition de la nouvelle image comme une image vierge
-		int imageRecaleeWidth, imageRecaleeHeight, imageRecaleeType
-        BufferedImage imageRecalee = new BufferedImage(imageRecaleeWidth, imageRecaleeHeight, imageRecaleeType);
+		BufferedImage imageRecalee = new BufferedImage(imageBrute.getWidth(), imageBrute.getHeight(), imageBrute.getType());
 		
-		for(int i=0; i<imagerecaleeWidth; i++) {
-	            for(int j=0; j<imagerecaleeHeight; j++) {  //On parcourt toute l'image
+		for(int i=0; i<imageRecalee.getWidth(); i++) {
+	            for(int j=0; j<imageRecalee.getHeight(); j++) {  //On parcourt toute l'image
 	            	
-	            	//Définition de tous les points nécessaires
-	            	(u,v) = phiInverse(i,j);
-	            	(x1,x2) = (floor(u), floor(v));
-	            	x2 = (x1+1);
-	            	y2 = x2;
+	            	//Definition de tous les points necessaires
+	            	u = (int)(i*Math.cos(0) - j*Math.sin(0));
+	            	v = (int)(i*Math.cos(0) + j*Math.sin(0));
+	            	x1=(int)(u);
+	            	y1= (int)(v);
+	            	x2 = x1+1;
+	            	y2 = y1;
 	            	x3 = x1;
 	            	y3 = (y1+1);
-	            	x4 = (x1+1);
-	            	y4 = (y1+1);
+	            	x4 = x2;
+	            	y4 = y3;
 	            	dx = (u-x1);
-	            	dy = (v-x2);
+	            	dy = (v-y1);
 	            	
 	            	//Et de leur couleur
-	            	rouge1 = new Color(imageBrute.getRGB(x2, y2)).getRed();
-	                vert1 = new Color(imageBrute.getRGB(x2, y2)).getGreen();
-	                bleu1 = new Color(imageBrute.getRGB(x2, y2)).getBlue();
-	                alpha1 = new Color(imageBrute.getRGB(x2, y2)).getAlpha();
+	            	rouge1 = new Color(imageBrute.getRGB(x1, y1)).getRed();
+	                vert1 = new Color(imageBrute.getRGB(x1, y1)).getGreen();
+	                bleu1 = new Color(imageBrute.getRGB(x1, y1)).getBlue();
+	                alpha1 = new Color(imageBrute.getRGB(x1, y1)).getAlpha();
 	                
 	                rouge2 = new Color(imageBrute.getRGB(x2, y2)).getRed();
 	                vert2 = new Color(imageBrute.getRGB(x2, y2)).getGreen();
@@ -79,11 +104,11 @@ public class ApplicationHomographie{
 	                bleu4 = new Color(imageBrute.getRGB(x4, y4)).getBlue();
 	                alpha4 = new Color(imageBrute.getRGB(x4, y4)).getAlpha();
 	                
-	                rouge = floor(dx*dy*rouge1 + (1-dx)*dy*rouge2 + dx*(1-dy)*rouge3 + (1-dx)*(1-dy)*rouge4);
-	                vert = floor(dx*dy*vert1 + (1-dx)*dy*vert2 + dx*(1-dy)*vert3 + (1-dx)*(1-dy)*vert4);
-	                bleu = floor(dx*dy*bleu1 + (1-dx)*dy*bleu2 + dx*(1-dy)*bleu3 + (1-dx)*(1-dy)*bleu4);
-	                alpha = floor(dx*dy*alpha1 + (1-dx)*dy*alpha2 + dx*(1-dy)*alpha3 + (1-dx)*(1-dy)*alpha4);
-	                gris = (0.21 * rouge + 0.71 * vert + 0.07 * bleu);
+	                rouge = (int)(dx*dy*rouge1 + (1-dx)*dy*rouge2 + dx*(1-dy)*rouge3 + (1-dx)*(1-dy)*rouge4);
+	                vert = (int)(dx*dy*vert1 + (1-dx)*dy*vert2 + dx*(1-dy)*vert3 + (1-dx)*(1-dy)*vert4);
+	                bleu = (int)(dx*dy*bleu1 + (1-dx)*dy*bleu2 + dx*(1-dy)*bleu3 + (1-dx)*(1-dy)*bleu4);
+	                alpha = (int)(dx*dy*alpha1 + (1-dx)*dy*alpha2 + dx*(1-dy)*alpha3 + (1-dx)*(1-dy)*alpha4);
+	                gris = (int) (0.21 * rouge + 0.71 * vert + 0.07 * bleu);
 
 	                //Coloration de l'image
 	                nouveauPixel = colorerPixel(alpha, gris, gris, gris);
@@ -92,7 +117,24 @@ public class ApplicationHomographie{
 	            }
 		 }       
 	     
-		return imageRecalee
+		return imageRecalee;
 	}
+
+	
+	 // colore un pixel avec Rouge, Vert, Bleu, Alpha avec les 8 bits
+    private static int colorerPixel(int alpha,int rouge, int vert, int bleu) {
+ 
+        int nouveauPixel = 0;
+        nouveauPixel += alpha;
+        nouveauPixel = nouveauPixel << 8;
+        nouveauPixel += rouge ;
+        nouveauPixel = nouveauPixel << 8;
+        nouveauPixel += vert;
+        nouveauPixel = nouveauPixel << 8;
+        nouveauPixel += bleu;
+ 
+        return nouveauPixel;
+ 
+    }
 	
 }
