4a2e6649f29e071fb92320d5f79b66efc464cca2
Nicolas Pontois <pontois.n@gmail.fr>
2015-03-03 16:40:21 +0100

    Nouvelle methode pour trier les points (les coins) et amelioration de l inversion de matrices

--- src/RecalageImage/EstimationHomographie/TrierPoint.java
+++ src/RecalageImage/EstimationHomographie/TrierPoint.java
@@ -9,14 +9,11 @@ public class TrierPoint {
 		Coins.set(0, A) ; Coins.set(1, B) ; Coins.set(2, C) ; Coins.set(3, D) ;
 		if(A.getX() < B.getX()) {
 			if(B.getX() < C.getX()) {
-				if(C.getX() < D.getX()) return Coins ;
-				else {
-					if(B.getX() < D.getX()) return Coins ;
-					else {
+				if(C.getX() > D.getX()) {
+					if(B.getX() > D.getX()) {
 						P = Coins.get(1) ;
 						Coins.set(1,Coins.get(3)) ;
 						Coins.set(3, P) ;
-						return Coins ;
 					}
 				}
 			}
@@ -26,14 +23,81 @@ public class TrierPoint {
 						P = Coins.get(1) ;
 						Coins.set(1,Coins.get(2)) ;
 						Coins.set(2,P) ;
-						return Coins ;
 					}
 					else {
-						if()
+						if(C.getX() < D.getX()) {
+							P = Coins.get(1) ;
+							Coins.set(1,Coins.get(2)) ;
+							Coins.set(2,P) ;
+						}
+						else {
+							P = Coins.get(1) ;
+							Coins.set(1,Coins.get(3)) ;
+							Coins.set(3,P) ;
+						}
 					}
+				}
+				else {
+					
+					
 				}
 			}
 		}
+	}
+	
+	public ArrayList<Point> TrierPoints2(Point A, Point B, Point C, Point D) {
+		ArrayList<Point> Coins = new ArrayList<Point>(4) ;
+		Point P = new Point() ; 
+		Coins.set(0, A) ; Coins.set(1, B) ; Coins.set(2, C) ; Coins.set(3, D) ;
+		double x = (A.getX() + B.getX() + C.getX() + D.getX()) / 4.0 ;
+		if(A.getX() < x) {
+			if(B.getX() < x) ;
+			else {
+				if(C.getX() < x) {
+					P = Coins.get(1) ;
+					Coins.set(1,Coins.get(2)) ;
+					Coins.set(2,P) ;
+				}
+				else {
+					P = Coins.get(1) ;
+					Coins.set(1,Coins.get(3)) ;
+					Coins.set(3,P) ;
+				}
+			}
+		}
+		else {
+			if(B.getX() > x) {
+				P = Coins.get(0) ;
+				Coins.set(0,Coins.get(2)) ;
+				Coins.set(2,P) ;
+				P = Coins.get(1) ;
+				Coins.set(1,Coins.get(3)) ;
+				Coins.set(3,P) ;
+			}
+			else {
+				if(C.getX() < x) {
+					P = Coins.get(0) ;
+					Coins.set(0,Coins.get(2)) ;
+					Coins.set(2,P) ;
+				}
+				else {
+					P = Coins.get(0) ;
+					Coins.set(0,Coins.get(3)) ;
+					Coins.set(3,P) ;
+				}
+			}
+		}
+		if(Coins.get(0).getY() < Coins.get(1).getY()) {
+			P = Coins.get(0) ;
+			Coins.set(0,Coins.get(1)) ;
+			Coins.set(1,P) ;
+		}
+		if(Coins.get(2).getY() < Coins.get(3).getY()) {
+			P = Coins.get(2) ;
+			Coins.set(2,Coins.get(3)) ;
+			Coins.set(3,P) ;
+		}
+		return Coins ;
 	}
 
 }
