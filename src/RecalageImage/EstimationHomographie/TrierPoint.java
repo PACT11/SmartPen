9ca288727c49bf6b7ef52beee491ee5eff39eb23
Nicolas Pontois <pontois.n@gmail.fr>
2015-03-03 18:26:28 +0100

    Modification de la methode TrierPoints apres test et supression de l autre methode pour trier les points

--- src/RecalageImage/EstimationHomographie/TrierPoint.java
+++ src/RecalageImage/EstimationHomographie/TrierPoint.java
@@ -1,54 +1,14 @@
+package RecalageImage.EstimationHomographie;
+
 import java.awt.Point ;
 import java.util.ArrayList ;
 
 public class TrierPoint {
-	
-	public ArrayList<Point> TrierPoints(Point A, Point B, Point C, Point D) {
-		ArrayList<Point> Coins = new ArrayList<Point>(4) ;
-		Point P = new Point() ;
-		Coins.set(0, A) ; Coins.set(1, B) ; Coins.set(2, C) ; Coins.set(3, D) ;
-		if(A.getX() < B.getX()) {
-			if(B.getX() < C.getX()) {
-				if(C.getX() > D.getX()) {
-					if(B.getX() > D.getX()) {
-						P = Coins.get(1) ;
-						Coins.set(1,Coins.get(3)) ;
-						Coins.set(3, P) ;
-					}
-				}
-			}
-			else {
-				if(A.getX() < C.getX()) {
-					if(B.getX() < D.getX()) {
-						P = Coins.get(1) ;
-						Coins.set(1,Coins.get(2)) ;
-						Coins.set(2,P) ;
-					}
-					else {
-						if(C.getX() < D.getX()) {
-							P = Coins.get(1) ;
-							Coins.set(1,Coins.get(2)) ;
-							Coins.set(2,P) ;
-						}
-						else {
-							P = Coins.get(1) ;
-							Coins.set(1,Coins.get(3)) ;
-							Coins.set(3,P) ;
-						}
-					}
-				}
-				else {
-					
-					
-				}
-			}
-		}
-	}
-	
-	public ArrayList<Point> TrierPoints2(Point A, Point B, Point C, Point D) {
+		
+	public static ArrayList<Point> TrierPoints(Point A, Point B, Point C, Point D) {
 		ArrayList<Point> Coins = new ArrayList<Point>(4) ;
 		Point P = new Point() ; 
-		Coins.set(0, A) ; Coins.set(1, B) ; Coins.set(2, C) ; Coins.set(3, D) ;
+		Coins.add(0, A) ; Coins.add(1, B) ; Coins.add(2, C) ; Coins.add(3, D) ;
 		double x = (A.getX() + B.getX() + C.getX() + D.getX()) / 4.0 ;
 		if(A.getX() < x) {
 			if(B.getX() < x) ;
